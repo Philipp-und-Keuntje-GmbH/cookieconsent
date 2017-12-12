5f4c1a11d47ab4d4006403afd9ece477c47b0db1
Lars Engellandt <lars.engellandt@philippundkeuntje.de>
2017-12-12 14:37:36 +0100

    **PUKCFG_research // Added Fadeout for hourglass and new unicef Img

--- src/pukcookie.js
+++ src/pukcookie.js
@@ -1,6 +1,6 @@
 var jsonObj = null;
 var cont = document.body;
-var amount='1.00';
+var amount = '1.00';
 
 function getjson(user_options) {
     var xhr = new XMLHttpRequest();
@@ -42,6 +42,7 @@ function displaypopup(user_options) {
         }
         return target;
     }
+
     function isPlainObject(obj) {
         // The code "typeof obj === 'object' && obj !== null" allows Array objects
         return typeof obj === 'object' && obj !== null && obj.constructor === Object;
@@ -59,26 +60,26 @@ function displaypopup(user_options) {
         },
 
         content: {
-                acceptButton: jsonObj.unicef.cookie.acceptButton,
-                dismissButton: jsonObj.unicef.cookie.dismissButton,
-                link: {
-                    text: jsonObj.unicef.cookie.link.text,
-                    href: jsonObj.unicef.cookie.link.href
-                },
-                copyCookie: jsonObj.unicef.cookie.copy,
-                close: jsonObj.unicef.cookie.close,
-                timer:jsonObj.unicef.cookie.timer,
-                copyDonate: jsonObj.unicef.donate.copy,
-                logosrc: jsonObj.unicef.donate.logo.src,
-                logosrcMini:jsonObj.unicef.donate.logo.srcmini,
-                logohref: jsonObj.unicef.donate.logo.href,
-                paypalImg: jsonObj.unicef.donate.paypalimg,
-                paypalLink: jsonObj.unicef.donate.paypallink,
-                copyWindow: jsonObj.unicef.donate.window.copy,
-                placeholderWindow:jsonObj.unicef.donate.window.placeholder,
-                successWindow:jsonObj.unicef.donate.success.text,
-                returntext:jsonObj.unicef.donate.returntext,
-                returnurl:jsonObj.unicef.donate.returnurl
+            acceptButton: jsonObj.unicef.cookie.acceptButton,
+            dismissButton: jsonObj.unicef.cookie.dismissButton,
+            link: {
+                text: jsonObj.unicef.cookie.link.text,
+                href: jsonObj.unicef.cookie.link.href
+            },
+            copyCookie: jsonObj.unicef.cookie.copy,
+            close: jsonObj.unicef.cookie.close,
+            timer: jsonObj.unicef.cookie.timer,
+            copyDonate: jsonObj.unicef.donate.copy,
+            logosrc: jsonObj.unicef.donate.logo.src,
+            logosrcMini: jsonObj.unicef.donate.logo.srcmini,
+            logohref: jsonObj.unicef.donate.logo.href,
+            paypalImg: jsonObj.unicef.donate.paypalimg,
+            paypalLink: jsonObj.unicef.donate.paypallink,
+            copyWindow: jsonObj.unicef.donate.window.copy,
+            placeholderWindow: jsonObj.unicef.donate.window.placeholder,
+            successWindow: jsonObj.unicef.donate.success.text,
+            returntext: jsonObj.unicef.donate.returntext,
+            returnurl: jsonObj.unicef.donate.returnurl
         }
     };
     if (isPlainObject(user_options)) {
@@ -91,35 +92,35 @@ function displaypopup(user_options) {
         cookie: {
             acceptButton: '<span class="pc-acceptbutton"><button class="cookie-accept-btn">' + options.content.acceptButton + '</button></span>',
             dismissButton: '<span class="pc-dismissButton">' + options.content.dismissButton + '</span>',
-            text: '<div id="cookie-desc" class="pc-message-popup">' + options.content.copyCookie + '<a class="pc-cookie-info" href="'+ options.content.link.href + '" target="_blank">'+options.content.link.text +'</a> <button class="cookie-accept-btn">' + options.content.acceptButton + '</button></div>',
+            text: '<div id="cookie-desc" class="pc-message-popup">' + options.content.copyCookie + '<a class="pc-cookie-info" href="' + options.content.link.href + '" target="_blank">' + options.content.link.text + '</a> <button class="cookie-accept-btn">' + options.content.acceptButton + '</button></div>',
             link: '<a aria-label="learn more about cookies" role=button tabindex="0" class="pc-link" href="' + options.content.link.href + '" rel="noopener noreferrer nofollow" target="_blank">' + options.content.link.text + '</a>',
             close: '<img class="pc-close" src="' + options.content.close + '">'
         },
         donate: {
-            copy: '<span class="pc-message-popup">' + options.content.copyDonate +'</span>',
+            copy: '<span class="pc-message-popup">' + options.content.copyDonate + '</span>',
             logo: '<a href="' + options.content.logohref + '" rel="noopener noreferrer nofollow" target="_blank"> <img src="' + options.content.logosrc + '" class="pc-donatelogo-popup"></a>',
-            logopaypal:'<a href="' + options.content.paypalLink + '" rel="noopener noreferrer nofollow" target="_blank"> <img src="' + options.content.paypalImg + '" class="pc-donatelogo-popup"></a>',
-            timer:'<img class="pc-timer" src="'+ options.content.timer +'">',
+            logopaypal: '<a href="' + options.content.paypalLink + '" rel="noopener noreferrer nofollow" target="_blank"> <img src="' + options.content.paypalImg + '" class="pc-donatelogo-popup"></a>',
+            timer: '<img class="pc-timer" src="' + options.content.timer + '">',
             donatebutton: '<img class="pc-donatebuttonimg" src="' + options.content.paypal + '">',
-            window:{
-                logo:'<a href="' + options.content.logohref + '" rel="noopener noreferrer nofollow" target="_blank"> <img src="' + options.content.logosrc + '" class="pc-windowlogo"></a>',
+            window: {
+                logo: '<a href="' + options.content.logohref + '" rel="noopener noreferrer nofollow" target="_blank"> <img src="' + options.content.logosrc + '" class="pc-windowlogo"></a>',
                 copy: '<div class="pc-message-window">' + options.content.copyWindow + '</div>',
-                submit: '<input type="text" placeholder="'+ options.content.placeholderWindow+'" class="pc-input-window">',
+                submit: '<input type="text" placeholder="' + options.content.placeholderWindow + '" class="pc-input-window">',
                 paypalbutton:
                 '<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_blank" class="paypalform">' +
-                '        <input type="hidden" name="cmd" value="_s-xclick">'+
-                '        <input type="hidden" name="image_url" value="https://i.pinimg.com/736x/94/d4/c9/94d4c90934ffcfc00e48bcb01bae8d5a--unicef-logo-a-well.jpg">'+
-                '        <input type="hidden" name="hosted_button_id" value="JGB6CLUXU4Q7U">'+
-                '        <input type="hidden" name="return" value="'+options.content.returnurl+'">' +
+                '        <input type="hidden" name="cmd" value="_s-xclick">' +
+                '        <input type="hidden" name="image_url" value="https://i.pinimg.com/736x/94/d4/c9/94d4c90934ffcfc00e48bcb01bae8d5a--unicef-logo-a-well.jpg">' +
+                '        <input type="hidden" name="hosted_button_id" value="JGB6CLUXU4Q7U">' +
+                '        <input type="hidden" name="return" value="' + options.content.returnurl + '">' +
                 '        <input type="hidden" name="charset" value="utf-8">' +
                 '        <input type="hidden" name="item_number" value="144001">' +
-                '        <input type="hidden" name="cbt" value="'+options.content.returntext+'">' +
+                '        <input type="hidden" name="cbt" value="' + options.content.returntext + '">' +
                 '        <input type="hidden" name="lc" value="DE">' +
-                '        <input type="hidden" name="image_url" value="'+options.content.logosrcMini+'">' +
+                '        <input type="hidden" name="image_url" value="' + options.content.logosrcMini + '">' +
                 '        <input type="image" src="https://www.sandbox.paypal.com/de_DE/DE/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="Jetzt einfach, schnell und sicher online bezahlen – mit PayPal.">' +
                 '        <img alt="" border="0" src="https://www.sandbox.paypal.com/de_DE/i/scr/pixel.gif" width="1" height="1">' +
-                '    </form>' ,
-                success:'<div class="pc-sucess-text">'+options.content.sucessWindow+ amount +'</div>'
+                '    </form>',
+                success: '<div class="pc-sucess-text">' + options.content.sucessWindow + amount + '</div>'
             }
         }
     };
@@ -131,7 +132,6 @@ function displaypopup(user_options) {
         //Appending the cookie notice on the body element
         divCookie.className = "pc-wrapper cookie active";
         divCookie.innerHTML += options.elements.cookie.text;
-        divCookie.innerHTML += options.elements.cookie.acceptButton;
         cont.appendChild(divCookie);
 
         // Eventhandler on button
@@ -216,24 +216,25 @@ function displaypopup(user_options) {
         //         }
         //     }, '.pc-paypal-window');
         // });
-/*
-        function tbd(){
-            //adding a Eventlistener to input field to override the
-            var submit = donatewindow.getElementsByClassName("pc-input-window");
-            submit[0].addEventListener("keyup",function(){
-                console.log(submit[0].selectionStart, submit[0].selectionEnd);
-                if (this.value) {
-                    if (this.value.substr(-1) !== "€") {
-                        this.value += "€"
-                    }
-                    if(this.selectionStart === this.value.length){
-                        this.value.setSelectionRange(submit[0].value.length -1, submit[0].value.length -1)
-                    }
+        /*
+                function tbd(){
+                    //adding a Eventlistener to input field to override the
+                    var submit = donatewindow.getElementsByClassName("pc-input-window");
+                    submit[0].addEventListener("keyup",function(){
+                        console.log(submit[0].selectionStart, submit[0].selectionEnd);
+                        if (this.value) {
+                            if (this.value.substr(-1) !== "€") {
+                                this.value += "€"
+                            }
+                            if(this.selectionStart === this.value.length){
+                                this.value.setSelectionRange(submit[0].value.length -1, submit[0].value.length -1)
+                            }
+                        }
+                    });
                 }
-            });
-        }
-*/
+        */
     }
+
     /*
     function removewindow(classname){
         cont.removeChild(classname);
@@ -242,7 +243,20 @@ function displaypopup(user_options) {
     function acceptclick() {
         setcookie(options.cookieMeta.name, options.cookieMeta.value, options.cookieMeta.path, options.cookieMeta.domain, options.cookieMeta.expiryDays);
         switchstatus();
-        //setTimeout(closepopup, 10000);
+        setTimeout(closepopup, 10000);
+        var hourglass = document.getElementsByClassName("pc-timer");
+        fadeOut(hourglass[0]);
+    }
+
+    function fadeOut(image) {
+        var opacity = 1;
+        var timer = setInterval(function () {
+            if (opacity < 0.1) {
+                clearInterval(timer);
+            }
+            image.style.opacity = opacity;
+            opacity -= 0.1;
+        }, 1000);
     }
 
     function setcookie(name, value, path, domain, expiryDays) {
@@ -276,6 +290,7 @@ function displaypopup(user_options) {
         var cookie = document.getElementsByClassName("cookie");
         cookie[0].classList.remove("active");
     }
+
     /*
     function showendscreen(name, value){
         console.log(name,value);
@@ -297,9 +312,9 @@ function displaypopup(user_options) {
 (function (pc) {
     if (pc.hasInitialised) return;
 
-    pc.initialise=function(user_options) {
-        var checkcookie="CookieforGood";
-        if(user_options.cookieMeta.name){
+    pc.initialise = function (user_options) {
+        var checkcookie = "CookieforGood";
+        if (user_options.cookieMeta.name) {
             checkcookie = user_options.cookieMeta.name;
         }
         if (document.cookie.search(checkcookie)) {
